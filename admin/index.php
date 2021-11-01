72f21cb6a5eeaa8be1a80aa197bebd1e8d30e482
Shahriar Ahmed <you@example.com>
2021-11-01 18:49:00 +0600

    init

---
+++ admin/index.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+        <title>Food Ordering and Restaurant Booking System Admin Dashboard</title>
+        <link rel="stylesheet" href="../css/admin.css">
+    </head>
+
+    <body>
+        <h1>Admin Panel</h1>
+        <!-- Menu Section Starts --->
+        <div class="menu">
+            Menu
+        </div>
+        <!-- Menu Section Ends --->
+
+        <!-- Main Content Section Starts --->
+        <div class="main-content">
+            Main Content
+        </div>
+        <!-- Main Content Section Ends --->
+
+        <!-- Footer Section Starts --->
+        <div class="footer">
+            Footer
+        </div>
+        <!-- Footer Section Ends --->
+
+    </body>
+</html>
